ebacbea2587a13d529ce31274ef0090d378dd908
EvanArthur <edarth21@colby.edu>
2021-01-15 11:02:55 -0500

    creating song scraper

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+playlists = sp.user_playlists("spotify")
+
+
+while playlists:
+    for i, playlist in enumerate(playlists["items"]):
+        print(
+            "%4d %s %s"
+            % (i + 1 + playlists["offset"], playlist["uri"], playlist["name"])
+        )
+        result = sp.playlist_items(playlist["id"])
+        for song in result["items"]:
+            if song["track"] != None:
+                # print(song["track"].keys())
+                metrics = sp.audio_features(tracks=[song["track"]["id"]])
+                print(metrics[0].keys())
+                # print(song["track"]["id"])
+                # print(song["track"]["name"])
+
+    if playlists["next"]:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
